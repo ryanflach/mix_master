9ea51cc2f2fcdb1ca498d3e0d59ce1ef9ada1f1d
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-15 21:31:11 -0600

    Add controller tests

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -4,16 +4,20 @@ FactoryGirl.define do
     image_path  "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
   end
 
+  factory :song do
+    title
+    artist
+  end
+
+  factory :playlist do
+    name "MyPlaylist"
+  end
+
   sequence :name do |n|
     "#{n} Artist"
   end
 
   sequence :title, ["A", "C", "B"].cycle do |n|
     "#{n} Title"
-  end
-
-  factory :song do
-    title
-    artist
   end
 end
